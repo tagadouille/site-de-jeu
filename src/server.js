39f8b0488a9cc7a5b02163ee9c53aa56b22550ee
daie <elias.dai@etu.u-paris.fr>
2026-03-12 12:55:01 +0100

    Ajout du formulaire de connexion et du javascript associé, ajout du fichier jquery et d'un fichier js gérant la page de connexion.

--- src/server.js
+++ src/server.js
@@ -1,16 +1,27 @@
-const express = require('express');
+/*------------------------IMPORT-----------------------*/
+import express from "express";
+import { run } from "./connex.js";
+
+/*------------------------SERVER CONFIG-----------------------*/
 const app = express();
+const port = 4321;
 
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    
+/*------------------------ROUTES-----------------------*/
+app.get("/", (req, res) => {
+    res.redirect("signin");
 });
 
-app.post('/', (req, res) => {
-    
+app.post("/", (req, res) => {
+    res.send("POST received");
 });
 
-app.listen(4321);
+run({ app: app, action: "localhost:" + port });
+
+/*------------------------LISTEN-----------------------*/
+app.listen(port, () => {
+    console.log("Server launched on : http://localhost:" + port);
+});
