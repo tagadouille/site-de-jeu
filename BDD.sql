01e9c0cd78ff7325f7b1e83fc1ae3e0bc959b992
daie <elias.dai@etu.u-paris.fr>
2026-03-27 22:26:51 +0100

    Correction du fichier SQL

--- BDD.sql
+++ BDD.sql
@@ -1,42 +1,43 @@
 /*-----------DROP TABLES---------------*/
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS played_games;
+DROP TABLE IF EXISTS fav_games;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS users;
 
 /*-----------TABLES---------------*/
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(255) NOT NULL,
-  `firstname` VARCHAR(255) NOT NULL,
-  `lastname` VARCHAR(255) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `isOccuped` BOOLEAN NOT NULL,
-  `isConnected` BOOLEAN NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL UNIQUE,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  is_occupied BOOLEAN NOT NULL,
+  is_connected BOOLEAN NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS 'Games' (
-  `name` VARCHAR(255) NOT NULL,
-  `description` TEXT NOT NULL,
-  PRIMARY KEY (`name`)
+CREATE TABLE games (
+  name VARCHAR(255) PRIMARY KEY,
+  description TEXT NOT NULL
 );
 
 /*-----------ASSOCIATIONS---------------*/
 
-CREATE TABLE IF NOT EXISTS 'FavGames' (
-  `user_id` INT NOT NULL,
-  `game_name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`user_id`, `game_name`),
-  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
-  FOREIGN KEY (`game_name`) REFERENCES `Games`(`name`) ON DELETE CASCADE
+CREATE TABLE fav_games (
+  user_id INTEGER NOT NULL,
+  game_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (user_id, game_name),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (game_name) REFERENCES games(name) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS 'PlayedGames' (
-  `user_id` INT NOT NULL,
-  `game_name` VARCHAR(255) NOT NULL,
-  'NumberOfWins' INT NOT NULL,
-  'NumberOfMatches' INT NOT NULL,
-  PRIMARY KEY (`user_id`, `game_name`),
-  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
-  FOREIGN KEY (`game_name`) REFERENCES `Games`(`name`) ON DELETE CASCADE
+CREATE TABLE played_games (
+  user_id INTEGER NOT NULL,
+  game_name VARCHAR(255) NOT NULL,
+  number_of_wins INTEGER NOT NULL,
+  number_of_matches INTEGER NOT NULL,
+  PRIMARY KEY (user_id, game_name),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (game_name) REFERENCES games(name) ON DELETE CASCADE
 );
