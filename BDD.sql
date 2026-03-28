a2d5690f235dd6bdbbdf6776ebb07a4c67abe0fd
daie <elias.dai@etu.u-paris.fr>
2026-03-28 12:34:24 +0100

    Remplissage de la base avec quelques données

--- BDD.sql
+++ BDD.sql
@@ -41,3 +41,24 @@ CREATE TABLE played_games (
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   FOREIGN KEY (game_name) REFERENCES games(name) ON DELETE CASCADE
 );
+
+/*-----------INSERTIONS---------------*/
+INSERT INTO users (username, firstname, lastname, email, password, is_occupied, is_connected) VALUES
+  ('john_doe', 'John', 'Doe', 'john@mail.com', 'password123', false, false),
+  ('jane_smith', 'Jane', 'Smith', 'jane@mail.com', 'password456', true, false);
+
+
+INSERT INTO games (name, description) VALUES
+  ('Tic Tac Toe', 'A strategic  game where players must align their forms.'),
+  ('Power 4', 'A strategic game where players use gravity to align their pieces.');
+
+
+INSERT INTO fav_games (user_id, game_name) VALUES
+  (1, 'Tic Tac Toe'),
+  (1, 'Power 4'),
+  (2, 'Tic Tac Toe');
+
+INSERT INTO played_games (user_id, game_name, number_of_wins, number_of_matches) VALUES
+  (1, 'Tic Tac Toe', 5, 10),
+  (1, 'Power 4', 3, 8),
+  (2, 'Tic Tac Toe', 2, 5);
