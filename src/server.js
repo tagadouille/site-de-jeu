b5b081ee80346f3e440ea0e8998d6f65108fe796
daie <elias.dai@etu.u-paris.fr>
2026-04-02 11:46:25 +0200

    Début du server

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { runConnex } from "./formProcess/connex.js";
 import { runSign } from "./formProcess/sign.js";
 import { runProfile } from "./profile/profile.js";
 import { runIndex } from "./indexManager.js";
+import pg from "pg";
 
 /*------------------------SERVER CONFIG-----------------------*/
 const app = express();
@@ -13,6 +14,37 @@ const PATH = "http://localhost:" + PORT;
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+/*------------------------DATABASE CONFIG---------------------*/
+
+const pool = new pg.Pool({
+    user: 'site_admin',
+    host: 'localhost',
+    database: 'site_bdd',
+    password: 'P@risCite2026', //TODO : le chercher dans un fichier
+    port: 5432
+});
+
+
+async function operations() {
+    const client = await pool.connect();
+    // attente du résultat de la requête :
+    let res = await client.query("SELECT * FROM rendezvous");
+    // chaque nom de colonne correspond à un nom de propriété de row :
+    for (row of res.rows) {
+        console.log(row.date);
+        console.log(row.heure);
+        console.log(row.lieu);
+    }
+    // ...
+    // libération du client :
+    client.release();
+    // retour facultatif d'un résultat :
+    return res.rows;
+};
+operations()
+    .then(resultat => { console.log(resultat) })
+    .catch(err => console.err(err.stack)); // si une erreur se produit.
 
 /*------------------------ROUTES-----------------------*/
 
