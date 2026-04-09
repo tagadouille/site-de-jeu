1156c89bc6e4dbe548e172c931534ab9bcd3b8d8
gregoirn <noa.gregoire-mellier@etu.u-paris.fr>
2026-04-09 11:42:53 +0200

    Fusion connex sign

--- src/public/form/formConnex.js
+++ src/public/form/formConnex.js
@@ -1,51 +1,7 @@
-/*------------------RECUPERATION----------------------*/
-let username = $("#username");
-let email = $("#email");
 
-let password = $("#password");
-let submit = $("#submit");
+import { display_pass } from "./formFusion.js";
+/*------------------RECUPERATION----------------------*/
+
 
 /*------------------DOM MANIPULATION----------------------*/
-$(document).ready(function () {
-
-    submit_form();
-
-    // Display the password
-    $("#aff").on("mousedown", function () {
-
-        if(!$(this).prop("checked")) {
-            password.each(function () {
-                $(this).attr("type", "text");
-            });
-        }
-        else {
-            password.each(function () {
-                $(this).attr("type", "password");
-            });
-        }
-    });
-
-    // Verification if each input are filled
-    $("form").on("keyup", function () {
-
-        submit_form();
-    });
-});
-
-function submit_form() {
-
-    let valid_field_count = 0;
-    let field_count = 0;
-
-    $("form input:not([type='submit'])").each(function () {
-
-        if($(this).val() !== "") {
-            valid_field_count++;
-            $(this).css("border", "solid black 1px");
-        }
-        else {
-            $(this).css("border", "solid red 2px");
-        }
-        field_count++;
-    });
-}
+display_pass()
